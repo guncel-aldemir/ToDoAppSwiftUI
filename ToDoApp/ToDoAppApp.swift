9dbf11498bbe0ecf1d5dcb0e2fc3ca9a4b4f1ce2
güncel aldemir <guncelaldemir@gmail.com>
2024-03-01 21:10:47 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by aypro on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
